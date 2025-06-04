44f975b8574067160302d9fe0b73df5ec31098e7
Alberto Corona <muqui@hotmail.com>
2025-06-04 12:34:58 -0600

    add config mongoDB

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+
+import server from './server'
+
+const port  = process.env.PORT || "4000"
+
+server.listen(port, () => {
+    console.log( `Servidor Funcionando en el puerto: ${port} `) 
+})
---
+++ src/router.ts
@@ -0,0 +1,9 @@
+import { Router } from "express"
+
+ const router  = Router();
+
+ router.post('/auth/register', (req, res)=>{
+    console.log(req.body);
+ })
+  
+export default router;
